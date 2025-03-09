1ac2a019e8262f616a4354317031971c664d37fa
ekworsham <145511698+ekworsham@users.noreply.github.com>
2025-03-08 19:52:30 -0500

    w01 individual activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let productArray = getLocalStorage("so-cart") || [];
+  productArray.push(product);
+  setLocalStorage("so-cart", productArray);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
