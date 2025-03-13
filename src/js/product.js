5d2db31a9f41e9f35a8b13e30fac5be2b5e8edc5
didieravz <didiervz@hotmail.com>
2025-03-13 18:33:46 -0500

    complete team activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,27 +1,28 @@
 import { setLocalStorage } from "./utils.mjs";
-import { getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import { getParam } from "./utils.mjs";
+// Import aditional functions
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+// Obtener el ID del producto de la URL utilizando la función getParam
+const productId = getParam("product");
+console.log("we show the idProduct: ", productId); // debería mostrar '880RR' si la URL es correcta.
 
-function addProductToCart(product) {
-  // Retrieve the current cart (if it exists)
-  let cartItems = getLocalStorage("so-cart");
-  // If cartItems is not array, initialize as an empty array
-  if (!Array.isArray(cartItems)) {
-    cartItems = [];
-  }
-  // Add the produc o produts
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
+//New instance of product details
+const productDetails = new ProductDetails(productId, dataSource);
+
+// call the metod init from ProductDetails
+// Llamar al método init() para inicializar todo
+productDetails.init();
+
 // add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// add listener to Add to Cart button ctr+k ctr+c --> commet ; ctr+k ctr+u
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
