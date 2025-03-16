2624a4d163a126bf565c979988e69f4e452c7e32
pjworsham <145511699+pjworsham@users.noreply.github.com>
2025-03-16 18:16:31 -0400

    wk02 final unpdate

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,3 +1,17 @@
+
+function productCardTemplate(product) {
+  return `<li class="product-card">
+  <a href="product_pages/index.html?product=${product.Id}">
+  <img src="${product.Image}" alt="Image of ${product.Name}"/>
+  <h3 class="card__brand">${product.Brand.Name}</h3>
+  <h2 class="card__Name">${product.Name}</h2>
+  <p class="product-card__price">$${product.FinalPrice}</p>
+  </a>
+  </li>`
+  
+}
+
+
 export default class ProductList {
   constructor(category, dataSource, listElement) {
     this.category = category;
@@ -7,16 +21,9 @@ export default class ProductList {
   async init() {
     const list = await this.dataSource.getData();
   }
+  renderList(list) {
+    const htmlStrings = list.map(productCardTemplate);
+    this.listElement.insertAdjacentHTML ("afterbegin", htmlStrings.join(""));
+  }
 }
 
-function productCardTemplate(product) {
-  return `<li class="product-card">
-    <a href="product_pages/?product=">
-      <img src="" alt="Image of ">
-      <h2 class="card__brand"></h2>
-      <h3 class="card__name"></h3>
-      <p class="product-card__price">$</p>
-    </a>
-  </li>`
-
-}
