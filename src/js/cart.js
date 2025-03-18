e0578f287ef10b5434a9e0e4ee0d8c8e4a4c3de1
pjworsham <145511699+pjworsham@users.noreply.github.com>
2025-03-17 21:12:55 -0400

    fixed cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,8 +2,12 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    if (cartItems && Array.isArray(cartItems)) {
+      const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+      document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    }
+  
+  
 }
 
 function cartItemTemplate(item) {
