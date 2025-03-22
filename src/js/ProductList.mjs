fc5f339e7b60dfb57fbcdd071b4b9980d0896297
ekworsham <145511698+ekworsham@users.noreply.github.com>
2025-03-22 19:54:35 -0400

    update

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -25,10 +25,11 @@ export default class ProductList {
 
     async init() {
         // our dataSource will return a Promise...so we can use await to resolve it.
-        const list = await this.dataSource.getData();
+        const list = await this.dataSource.getData(this.category);
         // console.log(list); // Agrega esta línea para ver qué datos estás obteniendo
         // render the list
         this.renderList(list);
+        document.querySelector(".title").textContent = this.category;
     }
     // render after doing the first stretch
     renderList(list) {
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,15 +1,16 @@
-// import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, getParam} from "./utils.mjs";
 import ProductList from "./ProductList.mjs";
 import ProductData from "./ProductData.mjs";
 
-// loadHeaderFooter();
+loadHeaderFooter();
 
+const category = getParam("category");
 // Create a instance of ProductData to recuperate the data
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData();
 // obtain the ul element from index with its class
 const element = document.querySelector(".product-list");
 
 //create a new instance of ProductList
-const list = new ProductList("Tents", dataSource, element);
+const listing = new ProductList(category, dataSource, element);
 // Call the init method from ProductList
-list.init();
+listing.init();
