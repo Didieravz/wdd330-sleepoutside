7058ce3a7f4858ecdacf686ac9b42f8f6bd19357
SamuelReyes6671 <samuel.reyes.condori@email.com>
2025-03-28 23:44:49 -0500

    task4 completed

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -38,8 +38,13 @@ export default class ProductDetails {
   }
 }
 
-function productDetailsTemplate(product) {
-    return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+function productDetailsTemplate(product) { 
+  const originalPrice=product.SuggestedRetailPrice || product.FinalPrice //Se toma el precio original del producto. Si el producto no tiene un precio definido se una el FinalPrice
+  const discountedPrice=product.FinalPrice; //El precio real que pagará el cliente
+  const discount =originalPrice>discountedPrice
+  ? Math.round(((originalPrice - discountedPrice)/ originalPrice)*100) : 0;
+    
+  return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
       <h2 class="divider">${product.NameWithoutBrand}</h2>
       <img
         class="divider"
@@ -47,6 +52,7 @@ function productDetailsTemplate(product) {
         alt="${product.NameWithoutBrand}"
       />
       <p class="product-card__price">$${product.FinalPrice}</p>
+      ${discount > 0 ? `<p class="product-discount">🔥 ${discount}% OFF</p>` : ""}
       <p class="product__color">${product.Colors[0].ColorName}</p>
       <p class="product__description">
       ${product.DescriptionHtmlSimple}
